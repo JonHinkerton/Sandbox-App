169de3fa625012b886331ff2504f97527115a44f
Phil Bowen <phil.bowen@gmail.com>
2018-04-20 15:14:27 -0400

    next step is https://angular.io/tutorial/toh-pt5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
-
 import { ThingModule } from './Thing/ThingModule';
+
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { AppComponent } from './app.component';
+
+import { HeroService } from './hero.service';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageService } from './message.service';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule, FormsModule, ThingModule
   ],
-  providers: [],
+  providers: [ HeroService, MessageService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
